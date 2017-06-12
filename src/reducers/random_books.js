45b8fbddacfba154927c86b04f3cffe35f760bd1
robzdoom <jorgecruz.rd@gmail.com>
2017-06-12 10:30:53 -0700

    wired BooksReducer

---
+++ src/reducers/random_books.js
@@ -0,0 +1,10 @@
+export default function (){
+    return [
+        {title:'Batman Dark Knight'},
+        {title:'Futurama'},
+        {title:'Witcher Book'},
+        {title:'Evil Dentist'},
+        {title: 'The Crentist'}
+    ]
+}
+
